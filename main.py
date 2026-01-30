4e2943cd93a5cd5184c38e4109a8f509e3f786be
yoongijoa <157661956+yoongijoa@users.noreply.github.com>
2026-01-30 13:06:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from telegram.ext import Application, CommandHandler
 TOKEN = "8410288468:AAEhGY-A-8Zu1iBR8zaKGKP4UTSxjRQSpWY"
 
 async def start(update, context):
-    await update.message.reply_text("봇 정상 작동 중입니다 🚀")
+    await update.message.reply_text("Railway 봇 정상 작동중 🚀")
 
 def main():
     app = Application.builder().token(TOKEN).build()
